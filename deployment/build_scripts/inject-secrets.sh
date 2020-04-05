dafb864af2b073d225cabfa111e0d7180d8574ae
Ubuntu <ubuntu@ip-172-32-27-105.ec2.internal>
2020-04-05 04:51:44 +0000

    fix(deployment/secrets): correcting kubesecrets to map to correct context

--- deployment/build_scripts/inject-secrets.sh
+++ deployment/build_scripts/inject-secrets.sh
@@ -11,11 +11,11 @@ export $(cut -d= -f1 ./deployment/build_scripts/kube-secrets.txt)
  
  
 # Set kubernetes secrets
-./kubectl config set clusters.cs4300.k8s.mycourseindex.vpc.certificate-authority-data $CERTIFICATE_AUTHORITY_DATA
-./kubectl config set users.cs4300.k8s.mycourseindex.vpc.client-certificate-data "$CLIENT_CERTIFICATE_DATA"
-./kubectl config set users.cs4300.k8s.mycourseindex.vpc.client-key-data "$CLIENT_KEY_DATA"
-./kubectl config set users.cs4300.k8s.mycourseindex.vpc.password "$KUBE_PASSWORD"
-./kubectl config set users.cs4300.k8s.mycourseindex.vpc.net-basic-auth.password "$KUBE_PASSWORD"
+./kubectl config set clusters.k8s.ithaqueue.com.certificate-authority-data $CERTIFICATE_AUTHORITY_DATA
+./kubectl config set users.k8s.ithaqueue.com.client-certificate-data "$CLIENT_CERTIFICATE_DATA"
+./kubectl config set users.k8s.ithaqueue.com.client-key-data "$CLIENT_KEY_DATA"
+./kubectl config set users.k8s.ithaqueue.com.password "$KUBE_PASSWORD"
+./kubectl config set users.k8s.ithaqueue.com.net-basic-auth.password "$KUBE_PASSWORD"
  
 # set AWS secrets
 mkdir ~/.aws
